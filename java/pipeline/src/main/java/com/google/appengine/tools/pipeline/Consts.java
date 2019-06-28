687bcb161de0d308131ec24d87e8f4ab00289883
Alexander Urmuzov <urmuzov@gmail.com>
2019-06-28 18:14:32 +0500

    Property-based configuration: -Dcom.google.appengine.tools.pipeline.BLOB_BUCKET_NAME=project-pipeline-blobs -Dcom.google.appengine.tools.pipeline.SPANNER_PROJECT=project -Dcom.google.appengine.tools.pipeline.SPANNER_INSTANCE=cluster -Dcom.google.appengine.tools.pipeline.SPANNER_DATABASE=pipelines

--- java/pipeline/src/main/java/com/google/appengine/tools/pipeline/Consts.java
+++ java/pipeline/src/main/java/com/google/appengine/tools/pipeline/Consts.java
@@ -1,10 +1,17 @@
 package com.google.appengine.tools.pipeline;
 
 public final class Consts {
-    public static final String BLOB_BUCKET_NAME = "cloudaware-dev-pipeline-blobs";
-    public static final String SPANNER_PROJECT = "cloudaware-dev";
-    public static final String SPANNER_INSTANCE = "main-cluster";
-    public static final String SPANNER_DATABASE = "pipelines2";
+    public static final String BLOB_BUCKET_NAME;
+    public static final String SPANNER_PROJECT;
+    public static final String SPANNER_INSTANCE;
+    public static final String SPANNER_DATABASE;
+
+    static {
+        BLOB_BUCKET_NAME = System.getProperty("com.google.appengine.tools.pipeline.BLOB_BUCKET_NAME");
+        SPANNER_PROJECT = System.getProperty("com.google.appengine.tools.pipeline.SPANNER_PROJECT");
+        SPANNER_INSTANCE = System.getProperty("com.google.appengine.tools.pipeline.SPANNER_INSTANCE");
+        SPANNER_DATABASE = System.getProperty("com.google.appengine.tools.pipeline.SPANNER_DATABASE");
+    }
 
     private Consts() {
     }
